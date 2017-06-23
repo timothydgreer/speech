21c1ee0ea39c79d38917f1b889706c73ffc831f4
timothydgreer <timothydgreer@gmail.com>
2017-06-23 15:59:04 -0700

    Add my folders back

---
+++ cepstral_analysis/find_freq_cepstrum.m
@@ -0,0 +1,106 @@
+load('speech.mat');
+y = speech;
+
+%Hear the sound
+%speech_big = [speech, speech, speech, speech, speech, speech, speech,... 
+%speech, speech, speech, speech, speech, speech, speech, speech, speech];
+%sound(speech_big,10000);
+%Sounds like 'ah' at 120 Hz! Male voice
+figure
+plot(y)
+ham = hamming(250);
+%Make window
+windowed_y = y(1:250)'.*ham;
+figure
+plot(windowed_y)
+%Take fft
+ffy = fft(windowed_y,1024);
+%Find magnitude of fft
+for i = 1:length(ffy)
+    magffy(i) = abs(ffy(i));
+end
+%Find fourier of real cepstrum
+for i = 1:length(ffy)
+    logffy(i) = log(abs(ffy(i))^2);
+end
+%Get the real cepstrum
+ilogffy = ifft(logffy);
+%ilogffy(1) = 0;
+%ilogffy(end) = 0;
+ilogffy;
+figure
+plot(ilogffy(1:200))
+
+%Find the max of the cepstrum between 20 and 140. This is the peak
+[mag, ind] = max(abs(ilogffy(20:140)))
+%Correct for the index (starts at 20, not 0)
+trueind = ind+20
+%Find pitch by dividing by sampling rate
+pitch = (10000/trueind)
+
+
+
+%Part c)
+%Find the quantization levels
+quantized_pitch = 50:250/(-1+2^7):300;
+%Find the closest level to the pitch we found
+tmp_quant = abs(quantized_pitch - pitch);
+[err, ind_quant] = min(tmp_quant)
+quant_val = quantized_pitch(ind_quant)
+percent_err = abs(quant_val-pitch)/pitch
+
+
+max_c_quant = max(ilogffy(1:28));
+min_c_quant = min(ilogffy(1:28));
+quantized_c = min_c_quant:(max_c_quant-min_c_quant)/(-1+2^7):max_c_quant;
+%Use this function
+x = 1:28;
+myfun = 6./x;
+
+for i = 1:28
+    temp_quant = -myfun(i):(2*myfun(i)/(-1+2^7)):myfun(i);
+    temp_abs = abs(temp_quant - ilogffy(i));
+    [err, ind_temp_quant] = min(temp_abs);
+    cq(i) = temp_quant(ind_temp_quant);
+end
+
+%Results are GREAT
+figure
+plot(cq)
+figure
+plot(ilogffy(1:28))
+
+
+%Lifter!
+cq_res = cq;
+for i = 2:length(cq)
+    cq_res(i) = cq(i)*2;
+end
+
+
+%Uncomment two lines below to see what the lifter does to signal
+%figure
+%plot(cq_res)
+
+%Take fft
+refft = fft(cq_res,1024);
+
+%This was for visualization
+% for i = 1:length(refft)
+%     R(i) = abs(refft(i));
+%     ang(i) = angle(refft(i));
+% end
+% figure
+% subplot(2,1,1)
+% plot(R(1:500))
+% subplot(2,1,2)
+% plot(ang(1:500))
+
+%Exponentiate
+for i = 1:length(refft)
+    exprefft(i) = exp(refft(i));
+end
+%Take the ifft's real part to get the minimum-phase reconstruction
+new = real(ifft(exprefft));
+%figure
+%plot(new(1:200))
---
+++ potpourri/vad_fscores.m
@@ -0,0 +1,63 @@
+[mywav,Fs]=audioread('data/hw2vad.wav');
+groundTruth=load('data/vad.csv');
+vad=groundTruth(:,2)';
+%Fraction of voiced frames:
+disp(sum(vad)/length(vad) )%0.5183
+%Framerate (in Hz)
+disp(1/(groundTruth(2,1)-groundTruth(1,1))) %100Hz
+
+%==========================================================================
+%Part a)
+
+manualannot=rand(1,length(vad))>0.5;
+prec=sum(manualannot.*vad)/sum(manualannot);
+rec=sum(manualannot.*vad)/sum(vad);
+fmeas=2*prec*rec/(prec+rec);
+
+%==========================================================================
+%Part b)
+buf=buffer(mywav,Fs/100);buf(:,end)=[];
+ste=zeros(1,size(buf,2));
+epsilon=power(10,-5);
+L=size(buf,1);
+for framenum=1:length(ste)
+    ste(framenum)=10*log10(epsilon+(sum(power(buf(:,framenum),2)))/L);
+end
+
+
+allthresh=linspace(min(ste),max(ste),20);
+allprec=zeros(1,length(allthresh));
+allrec=zeros(1,length(allthresh));
+allfmeas=zeros(1,length(allthresh));
+
+for thresh=1:length(allthresh)
+    stevad=zeros(1,length(vad));
+    stevad(ste>allthresh(thresh))=1;
+    if sum(stevad)̃=0
+        allprec(thresh)=sum(stevad.*vad)/sum(stevad);
+    end
+    allrec(thresh)=sum(stevad.*vad)/sum(vad);
+    allfmeas(thresh)=2*allprec(thresh)*allrec(thresh)/(allprec(thresh)+allrec(thresh));
+end
+
+%==========================================================================
+%Part c)
+stzcr=zeros(1,size(buf,2));
+for framenum=1:length(stzcr)
+    stzcr(framenum)=mean(0.5*abs(diff(sign(buf(:,framenum)))));
+end
+allthresh=linspace(min(stzcr),max(stzcr),20); %mean(ste);
+allprec=zeros(1,length(allthresh));
+allrec=zeros(1,length(allthresh));
+allfmeas=zeros(1,length(allthresh));
+
+for thresh=1:length(allthresh)
+    stzcrvad=zeros(1,length(vad));
+    stzcrvad(stzcr<allthresh(thresh))=1;
+    %Voiced frames are expected to have lower ZCR
+    if sum(stzcrvad)̃=0
+        allprec(thresh)=sum(stzcrvad.*vad)/sum(stzcrvad);
+    end
+    allrec(thresh)=sum(stzcrvad.*vad)/sum(vad);
+    allfmeas(thresh)=2*allprec(thresh)*allrec(thresh)/(allprec(thresh)+allrec(thresh));
+end
